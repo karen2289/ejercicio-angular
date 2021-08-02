c8d8f02ad0f83944e4800d63752e32ac400a5b67
Karen Aguirre <latingirl_2289@hotmail.com>
2021-08-02 13:26:52 +0200

    Practicando Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HolaMundo';
+  listEstudiantes: any[] = [
+    { nombre: 'Karen Aguirre', estado: 'Promocionado' },
+    { nombre: 'Lukas Contreras', estado: 'Regular' },
+    { nombre: 'Patricia Moles', estado: 'Libre' },
+    { nombre: 'Linda Guzman', estado: 'Regular' },
+    { nombre: 'Delia Laguatasig', estado: 'Promocionado' },
+    { nombre: 'Delia Laguatasig', estado: 'Libre' }
+  ]
+  mostrar = true;
+
+  toogle(): void{
+    this.mostrar =! this.mostrar
+  }
 }
